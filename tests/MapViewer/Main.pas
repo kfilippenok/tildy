92b8eeb16cd7a064fcc0174b254b3b4abd5cb73e
kfilippenok <kfilippenok@infotecs-at.ru>
2024-10-29 18:09:20 +0500

    tests(MapViewer): Initial upload #10

---
+++ tests/MapViewer/Main.pas
@@ -0,0 +1,143 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, process, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls, ComCtrls, ActnList, ValEdit, ColorBox, EditBtn, mvMapViewer,
+  mvDLECache;
+
+type
+
+  { TfMain }
+
+  TfMain = class(TForm)
+    actStartStop: TAction;
+    ActionList: TActionList;
+    btnStartDownload: TButton;
+    chkProviderName: TCheckBox;
+    chkActive: TCheckBox;
+    chkCyclic: TCheckBox;
+    chkDoubleBuff: TCheckBox;
+    chkDebugTiles: TCheckBox;
+    chkPreviewTiles: TCheckBox;
+    chkUseThreads: TCheckBox;
+    chkZoomToCursor: TCheckBox;
+    DirectoryCache: TDirectoryEdit;
+    groupOther: TGroupBox;
+    groupCache: TGroupBox;
+    groupProviderTiles: TGroupBox;
+    groupProviderMap: TGroupBox;
+    ProviderName: TLabeledEdit;
+    MapView: TMapView;
+    ConsoleOutput: TMemo;
+    MVDECache: TMVDECache;
+    panMV: TPanel;
+    ProviderVariationsTiles: TComboBox;
+    ProviderVariationsMap: TComboBox;
+    mvScrollBox: TScrollBox;
+    tdScrollBox: TScrollBox;
+    Settings: TPageControl;
+    pageMapView: TTabSheet;
+    pageTilesDownloader: TTabSheet;
+    Splitter: TSplitter;
+    Splitter1: TSplitter;
+    procedure actStartStopExecute(Sender: TObject);
+    procedure chkActiveChange(Sender: TObject);
+    procedure chkCyclicChange(Sender: TObject);
+    procedure chkDebugTilesChange(Sender: TObject);
+    procedure chkDoubleBuffChange(Sender: TObject);
+    procedure chkPreviewTilesChange(Sender: TObject);
+    procedure chkUseThreadsChange(Sender: TObject);
+    procedure chkZoomToCursorChange(Sender: TObject);
+    procedure DirectoryCacheChange(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure ProviderVariationsMapChange(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  fMain: TfMain;
+
+implementation
+
+{$R *.lfm}
+
+{ TfMain }
+
+procedure TfMain.actStartStopExecute(Sender: TObject);
+begin
+  MapView.Active := not MapView.Active;
+
+  if MapView.Active then
+    actStartStop.Caption := 'Stop'
+  else
+    actStartStop.Caption := 'Start';
+end;
+
+procedure TfMain.chkActiveChange(Sender: TObject);
+begin
+  MapView.Active := chkActive.Checked;
+end;
+
+procedure TfMain.chkCyclicChange(Sender: TObject);
+begin
+  MapView.Cyclic := chkCyclic.Checked;
+end;
+
+procedure TfMain.chkDebugTilesChange(Sender: TObject);
+begin
+  MapView.DebugTiles := chkDebugTiles.Checked;
+end;
+
+procedure TfMain.chkDoubleBuffChange(Sender: TObject);
+begin
+  MapView.DoubleBuffered := chkDoubleBuff.Checked;
+end;
+
+procedure TfMain.chkPreviewTilesChange(Sender: TObject);
+begin
+  MapView.DrawPreviewTiles := chkPreviewTiles.Checked;
+end;
+
+procedure TfMain.chkUseThreadsChange(Sender: TObject);
+begin
+  MapView.UseThreads := chkUseThreads.Checked;
+end;
+
+procedure TfMain.chkZoomToCursorChange(Sender: TObject);
+begin
+  MapView.ZoomToCursor := chkZoomToCursor.Checked;
+end;
+
+procedure TfMain.DirectoryCacheChange(Sender: TObject);
+begin
+  MapView.CacheLocation := clCustom;
+  MapView.CachePath := DirectoryCache.Text;
+end;
+
+procedure TfMain.FormActivate(Sender: TObject);
+begin
+  chkActiveChange(Self);
+  chkCyclicChange(Self);
+  chkDebugTilesChange(Self);
+  chkDoubleBuffChange(Self);
+  chkPreviewTilesChange(Self);
+  chkUseThreadsChange(Self);
+  chkZoomToCursorChange(Self);
+  ProviderVariationsMapChange(Self);
+end;
+
+procedure TfMain.ProviderVariationsMapChange(Sender: TObject);
+begin
+  with ProviderVariationsMap do
+    MapView.MapProvider := Items[ItemIndex];
+end;
+
+end.
+
---
+++ tests/MapViewer/test_mapviewer.lpr
@@ -0,0 +1,26 @@
+program test_mapviewer;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  {$IFDEF HASAMIGA}
+  athreads,
+  {$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, Main
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  Application.{%H-}MainFormOnTaskbar:=True;
+  Application.Initialize;
+  Application.CreateForm(TfMain, fMain);
+  Application.Run;
+end.
+
